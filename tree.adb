0ae08b761d49a3d9fd08101f42dde46c63d38a11
sebimag <sebastien.anouilh@ensimag.grenoble-inp.fr>
2016-03-30 23:54:07 +0200

    tree.adb
    Squelette fonction et procedure

---
+++ tree.adb
@@ -0,0 +1,20 @@
+with Ada.Strings.Unbounded;
+use Ada.Strings.Unbounded;
+
+
+package body tree is
+
+	function New_Tree return Tree is
+	Begin
+
+	End;
+
+	procedure Insertion(T : in out Tree ; Word : in String) is
+	Begin
+
+	End;
+
+	procedure Search_And_Display(T : in Tree ; Letters : in String) is
+	Begin
+
+	End;
