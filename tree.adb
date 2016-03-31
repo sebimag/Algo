8a53eac946e82a64879631791e1d9e3d8157942d
Clement ARVIS <clement.avis@ensimag.grenoble-inp.fr>
2016-03-31 15:35:39 +0200

    new tree

--- tree.adb
+++ tree.adb
@@ -1,32 +1,1 @@
-with Ada.Strings.Unbounded;
-use Ada.Strings.Unbounded;
-
---git put et fromages!!!
-
-package body tree is
-	
-	type Nodes is array(Indice) of Tree;
-
-	type Node is record
-		Fils: Nodes;
-		nbrFils: Integer;
-	end record;
-	
-	function New_Tree return Tree is
-		--arbre : Node := (others => 0 ,nombreFils => 0);
-	Begin
-		--return arbre;
-		return NULL;
-	End;
-
-	procedure Insertion(T : in out Tree ; Word : in String) is
-	Begin
-		NULL;
-	End;
-
-	procedure Search_And_Display(T : in Tree ; Letters : in String) is
-	Begin
-		NULL;
-	End;
-
-end tree;
+package body tree is        function New_Tree return Tree is                T: Tree;        Begin                T := new Node'(fils => (others => null));                return T;        End;        procedure Insertion(T : in out Tree ; Word : in String) is        Begin        End;        procedure Search_And_Display(T : in Tree ; Letters : in String) is        Begin                NULL;        End;end tree;
--- tree.ads
+++ tree.ads
@@ -2,25 +2,21 @@ with Ada.Strings.Unbounded;
 use Ada.Strings.Unbounded;
 
 package Tree is
-	
-	M: constant := 3;
-	subtype Indice is Integer range 1..M;
-	type NombreFils is new Integer;
 
-	type Node is record
-		Fils : Nodes;
-		nbreFils : NombreFils;
-	end record;
-	
-	type Nodes is array(Indice) of Node;
+        M: constant := 3;
+        subtype Indice is Integer range 0..M;
 
-	type Tree is private;
-	function New_Tree return Tree;
-	procedure Insertion(T : in out Tree ; Word : in String);
-	procedure Search_And_Display(T : in Tree ; Letters : in String);
+
+        type Tree is private;
+        function New_Tree return Tree;
+        procedure Insertion(T : in out Tree ; Word : in String);
+        procedure Search_And_Display(T : in Tree ; Letters : in String);
 
 private
-
-	type Node;
-	type Tree is access Node;
+        type Node;
+        type Tree is access Node;
+        type Tab is array(Indice) of Tree;
+        type Node is record
+           Fils : Tab;
+        end record;
 end Tree;
