0508961afb9f81654aa1978af55fce07768a7cd8
Clement ARVIS <clement.avis@ensimag.grenoble-inp.fr>
2016-04-01 21:51:40 +0200

    optimisation

--- tree.adb
+++ tree.adb
@@ -1,61 +1,73 @@
 package body tree is
 
-	function New_Tree return Tree is
-	Begin
-		return new Node'((others => null), 'a', Empty_list);
-	End;
+        function New_Tree return Tree is
+        Begin
+                return new Node'((others => null), 'a', Empty_list);
+        End;
 
-	procedure Insertion(T : in out Tree ; Word : in String) is
-		tabl : alphabet := (others => 0);
-	begin
-		for I in Word'Range loop
-			tabl(Character'pos(Word(I))) := tabl(Character'pos(Word(I))) + 1;
-			if tabl(Character'pos(Word(I))) > M then
-				return;
-			end if;
-		end loop;
-		Insertion_Rec(T , Word, tabl);
-	End Insertion;
+        procedure CalcOccLetter(Word : in String; MotConforme : out Natural ; OccLetter : out Alphabet) is
+        begin
+           OccLetter := (others => 0);
+           MotConforme := 1;
+           for I in Word'Range loop
+                        OccLetter(Character'pos(Word(I))) := OccLetter(Character'pos(Word(I))) + 1;
+                        if OccLetter(Character'pos(Word(I))) > M then
+                           MotConforme := 0;
+                           OccLetter(Character'pos(Word(I))) := OccLetter(Character'pos(Word(I))) - 1;
+                        end if;
+           end loop;
+        end CalcOccLetter;
 
-
-	procedure Insertion_Rec(T : in out Tree ; Word : in String ; OccLetter : Alphabet) is
-	begin
-		if T.Letter = '{' then				--Si on est sur une feuille alors on ajoute dans la liste le WORD
-			T.L.Append(To_Unbounded_String(Word));
-		else	
-			if T.Fils(OccLetter(Character'Pos(T.Letter))) = null then		--Si le fils n'existe pas on le crée
-				T.Fils(OccLetter(Character'Pos(T.Letter))) := new Node'((others=>null), character'Succ(T.Letter), Empty_list);
-			end if;
-			Insertion(T.Fils(OccLetter(Character'Pos(T.Letter))), Word);	--Appel recursif vers le fils suivant
-		end if;
-	end Insertion_Rec;
-
-	procedure display(L : List) is
-		C: Liste.Cursor := First(L);
-	begin
-		while Has_Element(C) loop
-			Put("[" & To_String(Element(C)) & "] , ");
-			Next(C);
-		end loop;
-	end display;
+        procedure Insertion(T : in out Tree ; Word : in String) is
+           OccLetter : alphabet;
+           MotConforme  : Natural;
+        begin
+           CalcOccLetter(Word, MotConforme, OccLetter);
+           if MotConforme = 1 then
+              Insertion_Rec(T , Word, OccLetter);
+           end if;
+        End Insertion;
 
 
-	procedure Search_And_Display(T : in Tree ; Letters : in String) is
-		compteur : Natural := 0;
-	begin
-		if(T /= null and then T.Letter /= '{') then
-			for I in Letters'Range loop
-				If Letters(I) = T.Letter and compteur < M then
-					Search_And_Display(T.Fils(compteur), Letters);
-					Compteur := Compteur + 1;
-				end if;
-			end loop;
-			Search_And_Display(T.Fils(Compteur), Letters);
-		elsif T /= null then
-			display(T.L);
-		end if;
-	end Search_And_Display;
+        procedure Insertion_Rec(T : in out Tree ; Word : in String ; OccLetter : Alphabet) is
+        begin
+                if T.Letter = '{' then                          --Si on est sur une feuille alors on ajoute WORD dans la liste
+                        T.L.Append(To_Unbounded_String(Word));
+                else
+                        if T.Fils(OccLetter(Character'Pos(T.Letter))) = null then               --Si le fils n'existe pas on le crée
+                                T.Fils(OccLetter(Character'Pos(T.Letter))) := new Node'((others=>null), character'Succ(T.Letter), Empty_list);
+                        end if;
+                        Insertion(T.Fils(OccLetter(Character'Pos(T.Letter))), Word);    --Appel recursif vers le fils suivant
+                end if;
+        end Insertion_Rec;
+
+        procedure display(L : List) is
+                C: Liste.Cursor := First(L);
+        begin
+                while Has_Element(C) loop
+                        Put("[" & To_String(Element(C)) & "] , ");
+                        Next(C);
+                end loop;
+        end display;
+
+
+        procedure Search_And_Display(T : in Tree ; Letters : in String) is
+           OccLetter : Alphabet := (others=> 0);
+           MotConforme : Natural;
+        begin
+           CalcOccLetter(Letters, MotConforme , OccLetter);
+           Search_And_Display_rec(T , Letters, OccLetter);
+        end Search_And_Display;
+
+        procedure Search_And_Display_rec(T : in Tree ; Letters : in String ; OccLetter : in Alphabet) is
+        begin
+           if(T /= null and then T.Letter /= '{') then
+                        for I in 0..OccLetter(Character'Pos(T.Letter)) loop
+                           Search_And_Display(T.Fils(I), Letters);
+                        end loop;
+                elsif T /= null then
+                        display(T.L);
+                end if;
+        end Search_And_Display_rec;
 
 end tree;
---En Une Page maggle!
---Et il reste même de la place!
--- tree.ads
+++ tree.ads
@@ -2,26 +2,26 @@ with Ada.Strings.Unbounded, Ada.Containers.Doubly_Linked_Lists, Ada.Text_IO;
 use Ada.Strings.Unbounded, Ada.Text_IO;
 
 package Tree is
-	package Liste is new Ada.Containers.Doubly_Linked_Lists(Element_Type => Unbounded_String);
-	use Liste;
-	M: constant := 4;
-	subtype Indice is Integer range 0..M;
+        package Liste is new Ada.Containers.Doubly_Linked_Lists(Element_Type => Unbounded_String);
+        use Liste;
+        M: constant := 4;
+        subtype Indice is Integer range 0..M;
 
-	type Tree is private;
-	type Alphabet is array(97..122) of Natural;
-	function New_Tree return Tree;
-	procedure Insertion(T : in out Tree ; Word : in String);
-	procedure Search_And_Display(T : in Tree ; Letters : in String);
-	procedure Insertion_Rec(T : in out Tree ; Word : in String ; OccLetter : in Alphabet);
+        type Tree is private;
+        type Alphabet is array(97..122) of Natural;
+        function New_Tree return Tree;
+        procedure Insertion(T : in out Tree ; Word : in String);
+        procedure Search_And_Display(T : in Tree ; Letters : in String);
+        procedure Insertion_Rec(T : in out Tree ; Word : in String ; OccLetter : in Alphabet);
+        procedure Search_And_Display_rec(T : in Tree ; Letters : in String ; OccLetter : in Alphabet);
 
-	private
-	type Node;
-	type Tree is access Node;
-	type Tab is array(Indice) of Tree;
-	--type Alphabet is array(97..122) of Natural;
-	type Node is record
-		Fils : Tab;
-		letter: character;
-		L: Liste.List;
-	end record;
+        private
+        type Node;
+        type Tree is access Node;
+        type Tab is array(Indice) of Tree;
+        type Node is record
+                Fils : Tab;
+                letter: character;
+                L: Liste.List;
+        end record;
 end Tree;
