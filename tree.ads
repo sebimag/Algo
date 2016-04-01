7c24b07394672c404bf1526f38b8121f530f5d5f
sebimag <sebastien.anouilh@ensimag.grenoble-inp.fr>
2016-04-01 19:38:21 +0200

    ajout type alphabet

--- tree.ads
+++ tree.ads
@@ -4,18 +4,21 @@ use Ada.Strings.Unbounded, Ada.Text_IO;
 package Tree is
 	package Liste is new Ada.Containers.Doubly_Linked_Lists(Element_Type => Unbounded_String);
 	use Liste;
-	M: constant := 6;
+	M: constant := 4;
 	subtype Indice is Integer range 0..M;
 
 	type Tree is private;
+	type Alphabet is array(97..122) of Natural;
 	function New_Tree return Tree;
 	procedure Insertion(T : in out Tree ; Word : in String);
 	procedure Search_And_Display(T : in Tree ; Letters : in String);
+	procedure Insertion_Rec(T : in out Tree ; Word : in String ; OccLetter : in Alphabet);
 
 	private
 	type Node;
 	type Tree is access Node;
 	type Tab is array(Indice) of Tree;
+	--type Alphabet is array(97..122) of Natural;
 	type Node is record
 		Fils : Tab;
 		letter: character;
