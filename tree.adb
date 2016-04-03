cd31aa5c68aab59ae196475a972bb06cda1de345
Clement ARVIS <clement.avis@ensimag.grenoble-inp.fr>
2016-04-03 21:26:38 +0200

    suppression de calcOccLetter

--- tree.adb
+++ tree.adb
@@ -5,29 +5,17 @@ package body tree is
                 return new Node'((others => null), 'a', Empty_list);
         End;
 
-        procedure CalcOccLetter(Word : in String; MotConforme : out Natural ; OccLetter : out Alphabet) is
+        procedure Insertion(T : in out Tree ; Word : in String) is
+           OccLetter : Alphabet := (others => 0);
         begin
-           OccLetter := (others => 0);
-           MotConforme := 1;
            for I in Word'Range loop
                         OccLetter(Character'pos(Word(I))) := OccLetter(Character'pos(Word(I))) + 1;
                         if OccLetter(Character'pos(Word(I))) > M then
-                           MotConforme := 0;
-                           OccLetter(Character'pos(Word(I))) := OccLetter(Character'pos(Word(I))) - 1;
+                           return;
                         end if;
            end loop;
-        end CalcOccLetter;
-
-        procedure Insertion(T : in out Tree ; Word : in String) is
-           OccLetter : alphabet;
-           MotConforme  : Natural;
-        begin
-           CalcOccLetter(Word, MotConforme, OccLetter);
-           if MotConforme = 1 then
-              Insertion_Rec(T , Word, OccLetter);
-           end if;
+           Insertion_Rec(T , Word, OccLetter);
         End Insertion;
-
 
         procedure Insertion_Rec(T : in out Tree ; Word : in String ; OccLetter : Alphabet) is
         begin
@@ -45,17 +33,19 @@ package body tree is
                 C: Liste.Cursor := First(L);
         begin
                 while Has_Element(C) loop
-                        Put("[" & To_String(Element(C)) & "] , ");
+                        Put("[" & To_String(Element(C)) & "]  ");
                         Next(C);
                 end loop;
         end display;
 
-
         procedure Search_And_Display(T : in Tree ; Letters : in String) is
            OccLetter : Alphabet := (others=> 0);
-           MotConforme : Natural;
         begin
-           CalcOccLetter(Letters, MotConforme , OccLetter);
+           for I in Letters'Range loop
+              if OccLetter(Character'Pos(Letters(I))) < M then
+                OccLetter(Character'pos(Letters(I))) := OccLetter(Character'pos(Letters(I))) + 1;
+              end if;
+           end loop;
            Search_And_Display_rec(T , Letters, OccLetter);
         end Search_And_Display;
 
